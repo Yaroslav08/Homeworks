6038d94802662d777e0b238354c399d48c2565cb
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-23 11:18:36 +0300

    LW4 is ready

--- Homework/LW4/Program.cs
+++ Homework/LW4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace LW4
 {
@@ -6,7 +7,29 @@ namespace LW4
     {
         static void Main(string[] args)
         {
-            
+            var arr = new int[] { 1, 2, 4, 8, 16, 32, 64, 128 };
+            Console.WriteLine(GetCount(arr));
+        }
+
+        static int GetCount(params int[] arr)
+        {
+            int count = 0;
+            int currentElement = 0;
+            int? nextElement = 0;
+            int currentIndex = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                currentIndex = i;
+                if (currentIndex + 1 == arr.Length)
+                    break;
+                currentElement = arr[currentIndex];
+                nextElement = arr[++currentIndex];
+                if (currentElement * 2 == nextElement)
+                {
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
