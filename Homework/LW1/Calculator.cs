b6aaf2ebb4057ce3a7571302ae5fb9c406e1e529
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-23 10:16:50 +0300

    Added Calculator class

---
+++ Homework/LW1/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace LW1
+{
+    public class Calculator
+    {
+        private int commonLength;
+        private int firstSpeed;
+        private int secondSpeed;
+
+        public Calculator(int commonLength, int firstSpeed, int secondSpeed)
+        {
+            this.commonLength = commonLength;
+            this.firstSpeed = firstSpeed;
+            this.secondSpeed = secondSpeed;
+        }
+
+        public int GetMinutesOfWalk()
+        {
+            return commonLength / (firstSpeed + secondSpeed);
+        }
+    }
+}
