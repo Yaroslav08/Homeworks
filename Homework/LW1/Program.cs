aa31466bab7dd1d6ce4080d9dc0a7871b00322bf
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-23 10:35:38 +0300

    Changed LW1

--- Homework/LW1/Program.cs
+++ Homework/LW1/Program.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.Text;
+
 namespace LW1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Лабораторна робота №1, Варіант 14");
             Calculator calculator = new Calculator(60, 12, 15);
-            Console.WriteLine($"Time to execute task: {calculator.GetHoursOfWalk()} hours");
+            Console.WriteLine($"Час через який вони зустрінуться: {calculator.GetHoursOfWalk()} годин");
         }
     }
 }
