45e2953017749d7bfa02d46a6f7034fdb948c464
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-27 21:51:40 +0300

    Changed in LW4 from one array to multi array

--- Homework/LW4/Program.cs
+++ Homework/LW4/Program.cs
@@ -7,26 +7,35 @@ namespace LW4
     {
         static void Main(string[] args)
         {
-            var arr = new int[] { 1, 2, 4, 8, 16, 32, 64, 128 };
+            var arr = new int[,] { { 1, 2 }, { 4, 56 }, { 45, 46 }, { 13, 11 }, { 67, 5 } };
             Console.WriteLine(GetCount(arr));
         }
 
-        static int GetCount(params int[] arr)
+        static int GetCount(int[,] arr)
         {
             int count = 0;
-            int currentElement = 0;
-            int? nextElement = 0;
-            int currentIndex = 0;
-            for (int i = 0; i < arr.Length; i++)
+            int currentIndexI = 0, currentIndexJ = 0, currentElement = 0, nextElement = 0;
+            for (int i = 0; i < 5; i++)
             {
-                currentIndex = i;
-                if (currentIndex + 1 == arr.Length)
-                    break;
-                currentElement = arr[currentIndex];
-                nextElement = arr[++currentIndex];
-                if (currentElement * 2 == nextElement)
+                for (int j = 0; j < 2; j++)
                 {
-                    count++;
+                    currentIndexI = i;
+                    currentIndexJ = j;
+                    currentElement = arr[i, j];
+                    if (j == 1)
+                    {
+                        if (i == 4)
+                            break;
+                        nextElement = arr[(i) + 1, j];
+                    }
+                    else
+                    {
+                        nextElement = arr[i, (j) + 1];
+                    }
+                    if (currentElement + 1 == nextElement || currentElement - 1 == nextElement)
+                    {
+                        count++;
+                    }
                 }
             }
             return count;
