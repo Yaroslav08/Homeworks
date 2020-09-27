b0abf425c7d24fb92658ac92e1e8d1bca1408e9c
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-09-27 20:55:46 +0300

    Added some array in LW5

--- Homework/LW5/Program.cs
+++ Homework/LW5/Program.cs
@@ -6,7 +6,8 @@ namespace LW5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] arr = { { 0, 1, 2, 3 }, { 4, 5, 6, 7 }, { 8, 9, 10, 11 }, { 12, 13, 14, 15 } };
+
         }
     }
 }
