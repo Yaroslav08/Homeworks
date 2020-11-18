db95547a56a9f471e74addfa5297cd2c2fe07da4
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-11-18 16:53:44 +0200

    Added example of lw6

--- Homework/LW6/Program.cs
+++ Homework/LW6/Program.cs
@@ -8,6 +8,9 @@ namespace LW6
         static void Main(string[] args)
         {
             var data = new WorkData();
+            data.Reverse(12453654);
+            data.Reverse("Some string");
+            data.Reverse(new string[] { "Some", "body", "was", "told", "me" });
             data.Reverse(1235.235);
         }
         static void Sos()
