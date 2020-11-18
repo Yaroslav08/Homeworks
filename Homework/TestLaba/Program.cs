dd58a1a19553b78206e0c4564556f6057a394f1d
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2020-11-18 22:20:50 +0200

    Added testing func for Spotify API

--- Homework/TestLaba/Program.cs
+++ Homework/TestLaba/Program.cs
@@ -8,6 +8,10 @@ using YoutubeExplode.Converter;
 using YoutubeExplode.Videos.Streams;
 using TestLaba.Koatuu;
 using System.Text;
+using SpotifyAPI.Web;
+using SpotifyAPI.Web.Auth;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace TestLaba
 {
@@ -62,16 +66,16 @@ namespace TestLaba
             //var posts1 = await postRepos.GetPostsByUserIdAsync(1);
 
             //var posts2 = await postRepos.GetPostsByUserIdAsync(2);
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
+
+
+
+
+
+
+
+
+
+
             //var youtube = new YoutubeClient();
             //IYoutubeConverter _youtubeConverter = new YoutubeConverter();
             //var streamManifest = await youtube.Videos.Streams.GetManifestAsync("https://youtu.be/2zToEPpFEN8");
@@ -83,25 +87,85 @@ namespace TestLaba
             //var builder = new ConversionRequestBuilder("video.mp4").Build();
             //await youtube.Videos.DownloadAsync(streamInfos, builder);
             //Console.WriteLine("Video was downloaded");
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            KoatuuManager manager = new KoatuuManager();
-            await manager.LoadData();
-            var cities = manager.GetCitiesByName("М.Київ").OrderBy(d=>d.Name);
-            Console.WriteLine($"Founded {cities.Count()} cities");
-            foreach (var city in cities)
-            {
-                Console.WriteLine($"Id: {city.Id}, Name: {city.Name}");
-            }
+
+
+
+
+
+
+
+
+
+
+            //KoatuuManager manager = new KoatuuManager();
+            //await manager.LoadData();
+            //var cities = manager.GetCitiesByName("М.Київ").OrderBy(d=>d.Name);
+            //Console.WriteLine($"Founded {cities.Count()} cities");
+            //foreach (var city in cities)
+            //{
+            //    Console.WriteLine($"Id: {city.Id}, Name: {city.Name}");
+            //}
+
+
+
+
+
+
+
+
+
+
+
+
+            var spotify = new SpotifyClient("BQDrSdRsQKYrW3z28pyhmP2YLWfv-Ibe_wPrlAGPtFo8D2-hKLvbHQO8C59ZR-km3QaEhHaIhO-XKv-iCTV1x3h7zolXvXSaqKzkV6cM3ucGv2Yw5z_80tfi1rDVNujR21a5nL-XEyXcW7oa7mmCAiXx5i57e61gGTLuWBa7tcygkyLJlg");
+            var search = await spotify.Search.Item(new SearchRequest(SearchRequest.Types.All, "Roses"));
+            var tracks = search.Tracks.Items;
+            var firstTrack = tracks.FirstOrDefault();
+            Process.Start(new ProcessStartInfo(firstTrack.PreviewUrl) { UseShellExecute = true });
+
+
+
+
+
+            //var loginRequest = new LoginRequest(
+            //  new Uri("http://localhost:5000/callback"),
+            //  "913a4b9d961f4ea39d3f94e71afe065e",
+            //  LoginRequest.ResponseType.Code)
+            //{
+            //    Scope = new[] { Scopes.PlaylistReadPrivate, Scopes.PlaylistReadCollaborative }
+            //};
+            //var uri = loginRequest.ToUri();
+
+            //_server = new EmbedIOAuthServer(new Uri("http://localhost:5000/callback"), 5000);
+            //await _server.Start();
+
+            //_server.AuthorizationCodeReceived += OnAuthorizationCodeReceived;
+
+            //var request = new LoginRequest(_server.BaseUri, "913a4b9d961f4ea39d3f94e71afe065e", LoginRequest.ResponseType.Code)
+            //{
+            //    Scope = new List<string> { Scopes.UserReadEmail }
+            //};
+            //BrowserUtil.Open(uri);
+
+            //Console.ReadKey();
         }
+        //private static EmbedIOAuthServer _server;
+        //private static async Task OnAuthorizationCodeReceived(object sender, AuthorizationCodeResponse response)
+        //{
+        //    await _server.Stop();
+
+        //    var config = SpotifyClientConfig.CreateDefault();
+        //    var tokenResponse = await new OAuthClient(config).RequestToken(
+        //      new AuthorizationCodeTokenRequest(
+        //        "913a4b9d961f4ea39d3f94e71afe065e", "f16b272b37164b09a95cfc4aade536d1", response.Code, new Uri("http://localhost:5000/callback")
+        //      )
+        //    );
+
+        //    var spotify = new SpotifyClient(tokenResponse.AccessToken);
+        //    var user = await spotify.UserProfile.Current();
+        //    Console.WriteLine(user.Email);
+        //}
+
 
         static UserRepository CreateUserRepos()
         {
