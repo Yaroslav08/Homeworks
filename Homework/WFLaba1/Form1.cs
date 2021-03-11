1cd0840d04d2bbd6026b8e0235d4f010d533fe86
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-03-11 19:46:57 +0200

    Added WFLaba1

---
+++ Homework/WFLaba1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFLaba1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Load += (sender, e) =>
+              {
+                  for (int i = 1; i <= 16; i++)
+                  {
+                      Button btn = new Button();
+                      btn.Name = $"button{i}";
+                      btn.Text = $"{i}";
+                      btn.Size = new Size(30, 30);
+                      btn.Location = new Point(10, 30 * i);
+                      btn.Click += buttonClick;
+                      Controls.Add(btn);
+                  }
+              };
+        }
+
+
+
+
+        private void buttonClick(object sender, EventArgs e)
+        {
+            (sender as Button).Visible = false;
+        }
+
+    }
+}
