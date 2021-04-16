2ba43002a0374d630d38e5880fc359421d4a1b33
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-04-16 17:20:23 +0300

    WPFLaba 7 is ready

---
+++ Homework/WPFLaba7/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFLaba7
+{
+    public partial class MainWindow : Window
+    {
+        TreeFolder treeFolder = new TreeFolder(Directory.GetCurrentDirectory());
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateFolders_Click(object sender, RoutedEventArgs e)
+        {
+            if (!long.TryParse(tbtPath.Text, out var count))
+                return;
+            treeFolder.CreateTreeFolders(count);
+        }
+
+        private void getFilesListbtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbtPath.Text))
+                return;
+            var paths = Directory.GetFiles(tbtPath.Text);
+            if (paths.Length <= 0)
+                return;
+            filesListlb.ItemsSource = paths;
+        }
+
+        private async void filesListlb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var file = filesListlb.SelectedItem as string;
+            tbtText.Text = await new StreamReader(file).ReadToEndAsync();
+        }
+    }
+}
---
+++ Homework/WPFLaba7/TreeFolder.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFLaba7
+{
+    public class TreeFolder
+    {
+        private static Stack<string> folders;
+
+        public TreeFolder(string path)
+        {
+            folders = new Stack<string>();
+            folders.Push(path);
+        }
+
+        public void CreateTreeFolders(long numberFolders)
+        {
+            if (numberFolders != 0)
+            {
+                string folder = folders.Pop();
+                string path = folder + "\\" + Guid.NewGuid().ToString("N");
+                Directory.CreateDirectory(path);
+                folders.Push(path);
+                CreateTreeFolders(numberFolders - 1);
+            }
+        }
+    }
+}
