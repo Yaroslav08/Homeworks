fe3bbe789af3347c453bd1c678178551d0be9c30
Yaroslav Mudryk <38474523+Yaroslav08@users.noreply.github.com>
2021-06-15 20:11:12 +0300

    Laba 9 is ready

---
+++ Homework/WPFLaba9/MainWindow.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFLaba9
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly string FILE_NAME = "Text.txt";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public string Text => tbText.Text;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            if (btn.Name.Contains("D"))
+            {
+                DecryptData();
+                return;
+            }
+            EncryptData();
+        }
+
+        private void DecryptData()
+        {
+            Decrypt(Text);
+        }
+
+        private void EncryptData()
+        {
+            Encrypt();
+        }
+
+        private void Decrypt(string text)
+        {
+            using (FileStream fileStream = new(FILE_NAME, FileMode.OpenOrCreate))
+            {
+                using (Aes aes = Aes.Create())
+                {
+                    byte[] key =
+                    {
+                            0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                            0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16
+                    };
+                    aes.Key = key;
+                    byte[] iv = aes.IV;
+                    fileStream.Write(iv, 0, iv.Length);
+
+                    using (CryptoStream cryptoStream = new(fileStream, aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter encryptWriter = new(cryptoStream))
+                        {
+                            encryptWriter.WriteLine(text);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Encrypt()
+        {
+            using (FileStream fileStream = new(FILE_NAME, FileMode.Open))
+            {
+                using (Aes aes = Aes.Create())
+                {
+                    byte[] iv = new byte[aes.IV.Length];
+                    int numBytesToRead = aes.IV.Length;
+                    int numBytesRead = 0;
+                    while (numBytesToRead > 0)
+                    {
+                        int n = fileStream.Read(iv, numBytesRead, numBytesToRead);
+                        if (n == 0) break;
+
+                        numBytesRead += n;
+                        numBytesToRead -= n;
+                    }
+
+                    byte[] key =
+                    {
+                0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                0x09, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16
+            };
+
+                    using (CryptoStream cryptoStream = new(
+                       fileStream,
+                       aes.CreateDecryptor(key, iv),
+                       CryptoStreamMode.Read))
+                    {
+                        using (StreamReader decryptReader = new(cryptoStream))
+                        {
+                            string decryptedMessage = decryptReader.ReadToEnd();
+                            tbText.Text = decryptedMessage;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
